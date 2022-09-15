7638a3ad136eaa0c325101e8cdf40477d2b19bf5
Yudi Nakazaki <yudinakazaki@gmail.com>
2022-09-14 23:58:06 -0300

    feat: initial setup

---
+++ src/users/users.controller.ts
@@ -0,0 +1,8 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {}
+}
